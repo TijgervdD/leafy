c47cce7a91c85673185a6b3ef6ebbfffaea440a0
renatedwh <renate.dwh@gmail.com>
2025-12-18 14:27:47 +0100

    Update nema17.py

--- electronics/nema17.py
+++ electronics/nema17.py
@@ -1,19 +1,8 @@
-%"git clone https://github.com/adafruit/Adafruit-Motor-HAT-Pyt..."
+import time
+from adafruit_motorkit import MotorKit
 
-% "sudo raspi-config" (activate I2c interface)
+kit = MotorKit()
 
-% "cd Adafruit-Motor-HAT-Python-Library"
-
-% "sudo python setup.py install"
-
-% "cd examples"
-
-% edit line 20 of StepperTest.py to "myStepper = mh.getStepper(200, 2)"
-
-% python StepperTest.py
-
-while (True)
-    print("Single coil steps")
-    myStepper.step(100, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
-    myStepper.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
-    
+for i in range(100):
+    kit.stepper2.onestep()
+    time.sleep(0.01)
