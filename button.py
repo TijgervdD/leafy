fe174ed5b617840ffe39c1b8d1b4483228faf4d7
renatedwh <renate.dwh@gmail.com>
2026-01-29 21:33:52 +0100

    Update button.py

--- button.py
+++ button.py
@@ -1,17 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-PIN = 20
+PIN = 20 # Your start button
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-print("Testing button on BCM 20 (Physical 38). Press it now!")
 
 try:
-    while True:
-        if GPIO.input(PIN) == GPIO.LOW:
-            print("Button Pressed!")
-            time.sleep(0.2) # Debounce
-        time.sleep(0.01)
-except KeyboardInterrupt:
+    print("Testing Pin 20...")
+    GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    print("Success! GPIO Allocated.")
+finally:
     GPIO.cleanup()
