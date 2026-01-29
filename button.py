d7a4bac1ca4669f1e000ae2ffceff54fe3bfbbb5
renatedwh <renate.dwh@gmail.com>
2026-01-29 21:42:03 +0100

    Update button.py

--- button.py
+++ button.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
-import time
-
-PIN = 20 # Your start button
 GPIO.setmode(GPIO.BCM)
 
-try:
-    print("Testing Pin 20...")
-    GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    print("Success! GPIO Allocated.")
-finally:
-    GPIO.cleanup()
+# Test your pins one by one:
+GPIO.setup(23, GPIO.OUT) # Motor 1
+GPIO.setup(22, GPIO.OUT) # Motor 1
+GPIO.setup(24, GPIO.OUT) # EN 1
+GPIO.setup(27, GPIO.OUT) # Motor 2
+GPIO.setup(18, GPIO.OUT) # Motor 2
+GPIO.setup(17, GPIO.OUT) # EN 2
+GPIO.setup(5, GPIO.OUT)  # TRIG 1
+GPIO.setup(6, GPIO.IN)   # ECHO 1
+GPIO.setup(12, GPIO.OUT) # TRIG 2
+GPIO.setup(13, GPIO.IN)  # ECHO 2
+GPIO.setup(21, GPIO.OUT) # RELAY
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP) # START
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) # STOP
